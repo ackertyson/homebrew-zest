5067439ae0c0c4484b7663b54407e6928ca5e16d
Tyson Acker <ackertyson@gmail.com>
2026-03-19 19:06:09 -0500

    zest 0.1.2

--- Formula/zest.rb
+++ Formula/zest.rb
@@ -1,13 +1,13 @@
 class Zest < Formula
   desc "Animate your shell prompt into view with a choice of effects"
   homepage "https://github.com/ackertyson/zest"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/ackertyson/zest/releases/download/v#{version}/zest-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "7caef26ae97b97d133f4240d85bf8ca4666dfb885373ef8a0fbf064a50efb6b6"
+      sha256 "cdcc01859f3026d029c1fe759e02fdbe38dc5655bd1b716a95cc305d3910edc2"
     end
   end
 
